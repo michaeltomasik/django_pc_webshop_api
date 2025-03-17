5382bf4d7d350e0039dc7dd5d32672acbc9ad345
Darkchanze <nico.wittemann@t-online.de>
2025-03-17 15:16:45 +0100

    Troubleshooting added prints to fined server error 2

--- app/pc_components/apps.py
+++ app/pc_components/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+print("pre app pc components")
 class PcComponentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.pc_components'
+print("post app pc components")
