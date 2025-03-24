7c84ccfb775e3f1fd98af78e698ffbc9ef68517f
Michał Tomasik <shugo12345@gmail.com>
2025-03-24 23:45:00 +0100

    Update wsgi.py

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -13,7 +13,7 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 # Add the project root directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
