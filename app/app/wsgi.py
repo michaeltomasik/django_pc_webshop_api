82c9065e429f52c4b91273414b2a8a1696b1bf6f
Darkchanze <nico.wittemann@t-online.de>
2025-03-24 21:41:00 +0100

    change

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 # Add the project root directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
 print("end wsgi")
